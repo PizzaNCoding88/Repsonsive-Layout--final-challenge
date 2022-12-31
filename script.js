4ed3bcdb5b85de79a1a6e547bd46e4f1ace135dc
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2022-12-31 16:43:11 +0000

    navigation hiding completed

--- script.js
+++ script.js
@@ -11,52 +11,6 @@ function openMenu() {
 
 //HIDE NAVIGATION BAR
 
-// const nav = document.querySelector("header");
-// const navHeight = 69;
-
-// let lastScrollY = 0;
-
-// const delta = 10;
-
-
-// function scrolled() {
-//   let sy = window.scrollY;
-  
-//   if (Math.abs(lastScrollY - sy) > delta) {
-    
-//     if (sy > lastScrollY && sy > navHeight && !navigation.classList.contains("disp-none")) {
-//         nav.classList.add("nav-up-menu-open");
-//     } 
-    
-//     else if (sy > lastScrollY && sy > navHeight && navigation.classList.contains("disp-none") ) {
-//         nav.classList.add("nav-up");
-//     }
-
-//     else (sy < lastScrollY) {
-//       nav.classList.remove("nav-up");
-//     }
-   
-//    lastScrollY = sy 
-//   }
-// }
-
-
-// let didScroll = false;
-// window.addEventListener("scroll", function(e){
-//   didScroll = true;
-// });
-
-// setInterval(function() {
-//   if (didScroll) {
-//     scrolled();
-//     didScroll = false;
-//    }
-// }, 250);
-
-
-
-/////
-
 
 const nav = document.querySelector("header");
 const navHeight = 69;
